66004186babc1176572ada42f9c420e23abb925d
Pavel Borovinskiy <borovinskiy.job@gmail.com>
2022-03-25 22:35:23 +0300

    Сокращение кода

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,28 +1,30 @@
 function compareArrays(arr1, arr2) {
 
-    let result = arr1.length == arr2.length && arr1.every((value, idx) => value == arr2[idx]);
+    let result;
 
 
 
-    return result; // boolean
+    return result = arr1.length == arr2.length && arr1.every((value, idx) => value == arr2[idx]); // boolean
 }
 
 
 
 function advancedFilter(arr) {
-    let resultArr = arr.filter(positiveNumbers).filter(multiples).map(multiplication);
+    //let resultArr = arr.filter(positiveNumbers).filter(multiples).map(multiplication);
 
-    function positiveNumbers(arr) {
-        return arr > 0;
-    }
+    //    function positiveNumbers(arr) {
+    //         return arr > 0;
+    //     }
 
-    function multiples(arr) {
-        return arr % 3 === 0;
-    }
+    //     function multiples(arr) {
+    //         return arr % 3 === 0;
+    //     }
 
-    function multiplication(arr) {
-        return arr * 10;
-    }
-    // let resultArr = arr.filter((element) => element > 0 ).filter((element) => element % 3 === 0 ).map((element) => element *10);
-    return resultArr; // array
+    //     function multiplication(arr) {
+    //         return arr * 10;
+    //     } 
+    let resultArr;
+
+
+    return resultArr = arr.filter((element) => element > 0).filter((element) => element % 3 === 0).map((element) => element * 10); // array
 }
