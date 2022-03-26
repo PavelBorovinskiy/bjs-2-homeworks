e3a6a1c97a9ecf6ec7ab6adaa24624b86e765e67
Pavel Borovinskiy <borovinskiy.job@gmail.com>
2022-03-27 00:52:30 +0300

    Доработка домашнего задания

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-	this.name = name;
-	this.gender = gender;
-	this.age = age;
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
 
-Student.prototype.setSubject = function (subjectName) {
-	this.subject = subjectName;
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 }
 
-Student.prototype.addMark = function (mark) {
-	this.marks = mark;
-	if (this.marks === undefined) {
-		mark = []
-	} else {
+Student.prototype.addMark = function(mark) {
 
-	}
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function(...mark) {
+    this.marks = mark;
+}
+Student.prototype.getAverage = function() {
+
+    this.marks.reduce((sum, current) => sum + current) / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
 }
