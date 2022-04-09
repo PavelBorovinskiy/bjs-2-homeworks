c8ecbd1ad8ecaed329a16837e75eeb2d7915d7c6
Borovinskiy Pavel <borovinskiy.job@gmail.com>
2022-04-09 11:18:05 +0300

    Начало работы над ДЗ

---
+++ 7.async/task.js
@@ -0,0 +1,23 @@
+class AlarmClock {
+	constructor(alarmCollection, id) {
+		this.alarmCollection = [alarmCollection];
+		this.timerId = id;
+
+	}
+
+	addClock(time, callback, id) {
+
+	}
+
+	removeClock() {
+
+	}
+
+	getCurrentFormattedTime() {
+
+	}
+
+	start() {
+
+	}
+}
