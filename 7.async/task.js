fa31d0ac8306e780aa7c94ff45d869f0a85bf958
Borovinskiy Pavel <borovinskiy.job@gmail.com>
2022-04-10 09:15:28 +0300

    «Асинхронность» Будильник-колыбельная

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,12 +1,19 @@
 class AlarmClock {
-	constructor(alarmCollection, id) {
-		this.alarmCollection = [alarmCollection];
-		this.timerId = id;
+	constructor() {
+		this.alarmCollection = [];
+		this.timerId = null;
 
 	}
 
-	addClock(time, callback, id) {
+	addClock(time, action, id) {
+		if (!id) {
+			throw new Error('Параметр id не передан');
+		}
 
+		if (this.alarmCollection.some(alarm => alarm.id === id)) {
+			return console.error('Будильник с таким id уже существует');
+		}
+		this.alarmCollection.push({ id: id, time: time, callback: action })
 	}
 
 	removeClock() {
@@ -14,10 +21,44 @@ class AlarmClock {
 	}
 
 	getCurrentFormattedTime() {
+		return new Date().toLocaleTimeString("ru-Ru", {
+			hour: '2-digit',
+			minute: '2-digit',
+		});
 
 	}
 
 	start() {
 
+		function checkClock(alarm) {
+			if (alarm.time === this.getCurrentFormattedTime()) {
+				alarm.callback();
+			}
+		}
+		let checkClockBind = checkClock.bind(this);
+
+		if (!this.timerId) {
+			this.timerId = setInterval(() => {
+				this.alarmCollection.forEach(alarm => checkClockBind(alarm));
+			}, 1000);
+		}
+	}
+
+	stop() {
+		if (!this.timerId) {
+			clearInterval(this.timerId);
+			this.timerId = null;
+		}
+	}
+
+	printAlarms() {
+		this.alarmCollection.forEach(alarm => {
+			console.log(`Будильник №${alarm.id} заведен на ${alarm.time}`);
+		})
+	}
+
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
 	}
 }
