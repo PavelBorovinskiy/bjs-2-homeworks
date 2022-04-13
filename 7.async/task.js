e343e97d6bc8b17888e4b7ff7a47ba3d81140072
Borovinskiy Pavel <borovinskiy.job@gmail.com>
2022-04-13 11:14:51 +0300

    Добавление метода removeClock

--- 7.async/task.js
+++ 7.async/task.js
@@ -5,60 +5,21 @@ class AlarmClock {
 
 	}
 
-	addClock(time, action, id) {
+	addClock(time, callback, id) {
 		if (!id) {
-			throw new Error('Параметр id не передан');
+			throw new Error('Парметр id не передан')
 		}
 
 		if (this.alarmCollection.some(alarm => alarm.id === id)) {
-			return console.error('Будильник с таким id уже существует');
+			console.error('Будильник с таким id уже существует');
+			return;
 		}
-		this.alarmCollection.push({ id: id, time: time, callback: action })
+
+		this.alarmCollection.push({ time: time, callback: callback, id: id })
 	}
 
-	removeClock() {
-
+	removeClock(id) {
+		this.alarmCollection.filter(alarm => alarm.id === id);
+		return delete this.alarmCollection.id;
 	}
-
-	getCurrentFormattedTime() {
-		return new Date().toLocaleTimeString("ru-Ru", {
-			hour: '2-digit',
-			minute: '2-digit',
-		});
-
-	}
-
-	start() {
-
-		function checkClock(alarm) {
-			if (alarm.time === this.getCurrentFormattedTime()) {
-				alarm.callback();
-			}
-		}
-		let checkClockBind = checkClock.bind(this);
-
-		if (!this.timerId) {
-			this.timerId = setInterval(() => {
-				this.alarmCollection.forEach(alarm => checkClockBind(alarm));
-			}, 1000);
-		}
-	}
-
-	stop() {
-		if (!this.timerId) {
-			clearInterval(this.timerId);
-			this.timerId = null;
-		}
-	}
-
-	printAlarms() {
-		this.alarmCollection.forEach(alarm => {
-			console.log(`Будильник №${alarm.id} заведен на ${alarm.time}`);
-		})
-	}
-
-	clearAlarms() {
-		this.stop();
-		this.alarmCollection = [];
-	}
-}
+}	
